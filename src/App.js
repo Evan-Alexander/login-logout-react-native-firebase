6950c2b20528e7027f9e2db0bb5dc7dbcfef5fb7
Jason Brown <jasontbrown99@gmail.com>
2018-01-31 14:08:38 -0800

    login / logout with firebase and react native

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'API KEY GOES HERE',
+      authDomain: 'authentication-f8ba7.firebaseapp.com',
+      databaseURL: 'https://authentication-f8ba7.firebaseio.com',
+      projectId: 'authentication-f8ba7',
+      storageBucket: 'authentication-f8ba7.appspot.com',
+      messagingSenderId: '21853165100'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
